d5e19a5d25e53a9f8631bf87badc97f39e9bf505
John P <johnrprutton@gmail.com>
2024-07-06 15:20:57 +0200

    add autoscroll to carousel

--- src/app/(landing)/tech-stack.tsx
+++ src/app/(landing)/tech-stack.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import AutoScroll from "embla-carousel-auto-scroll"
 import { motion } from "framer-motion"
 
 import {
@@ -19,62 +20,69 @@ export const TechStack = () => (
     transition={{ duration: 0.5 }}
     className="mt-16"
   >
-    <div className="space-y-12">
-      {Object.entries({ languages, frameworks, tools }).map(
-        ([name, group], i) => (
-          <div key={name} className="">
-            <h2 className="mb-2 text-muted-foreground/50">
-              {name.toLocaleUpperCase()}
-            </h2>
+    {Object.entries({ languages, frameworks, tools }).map(
+      ([name, group], i) => (
+        <div key={name}>
+          <h2 className="mb-2 text-muted-foreground/50">
+            {name.toLocaleUpperCase()}
+          </h2>
 
-              <div className="!mt-0 flex flex-row flex-wrap gap-x-8 gap-y-12">
-                <Carousel
-                  className="w-full"
-                  opts={{
-                    dragFree: true,
-                    loop: true
-                  }}
+          <Carousel
+            plugins={[
+              AutoScroll({
+                speed: 0.5,
+                startDelay: 0,
+                stopOnInteraction: false,
+                direction: i === 1 ? "backward" : "forward"
+              })
+            ]}
+            opts={{
+              dragFree: true,
+              loop: true
+            }}
+            className="cursor-grab select-none"
+          >
+            <CarouselContent>
+              {group.map((icon) => (
+                <CarouselItem
+                  key={icon.slug}
+                  className="relative basis-1/3 pb-10 sm:basis-1/4 md:basis-1/5"
                 >
-                  <CarouselContent className="-ml-pl-20">
-                    {group.map((icon, index) => (
-                      <CarouselItem
-                        key={icon.slug}
-                        className="basis-1/5 pl-20 md:basis-1/12"
-                      >
-                        <div
-                          key={icon.slug}
-                          className="relative flex flex-col items-center"
-                        >
-                          <motion.div
-                            key={icon.slug}
-                            initial={{ opacity: 0, scale: 0 }}
-                            whileInView={{ opacity: 1, scale: 1 }}
-                            transition={{
-                              type: "spring",
-                              stiffness: 200,
-                              damping: 15
-                              // delay: index * 0.1
-                            }}
-                            viewport={{ once: false }}
-                            style={{
-                              backgroundColor: `color-mix(in lch, #${icon.hex} 10%, transparent)`
-                            }}
-                            className="relative overflow-clip rounded p-3"
-                          >
-                            <div
-                              style={{ background: "#" + icon.hex }}
-                              className="absolute size-10 rounded-full opacity-25 blur-lg"
-                            />
+                  <motion.div
+                    initial={{ opacity: 0, scale: 0 }}
+                    whileInView={{ opacity: 1, scale: 1 }}
+                    transition={{
+                      type: "spring",
+                      stiffness: 200,
+                      damping: 15
+                    }}
+                    viewport={{ once: false }}
+                    className="relative mx-auto w-fit"
+                  >
+                    <div
+                      style={{
+                        backgroundColor: `color-mix(in lch, #${icon.hex} 10%, transparent)`
+                      }}
+                      className="relative w-fit overflow-clip rounded p-3"
+                    >
+                      <div
+                        style={{ background: "#" + icon.hex }}
+                        className="absolute size-10 rounded-full opacity-25 blur-lg"
+                      />
 
                       <RenderIcon icon={icon} className="relative size-10" />
-                    </motion.div>
-                  </CarouselItem>
-                ))}
-              </CarouselContent>
-            </Carousel>
-          </div>
-        )
-      )}
-    </div>
+                    </div>
+
+                    <span className="absolute -bottom-2 left-1/2 w-[200%] -translate-x-1/2 translate-y-full text-center text-xs text-muted-foreground">
+                      {icon.title}
+                    </span>
+                  </motion.div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
+        </div>
+      )
+    )}
   </motion.section>
 )

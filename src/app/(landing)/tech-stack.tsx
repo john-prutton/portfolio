eb79a6e2669c2f6846ab7026ddd38f5c56fc504e
John P <johnrprutton@gmail.com>
2024-07-06 14:52:22 +0200

    remove tabs

--- src/app/(landing)/tech-stack.tsx
+++ src/app/(landing)/tech-stack.tsx
@@ -9,7 +9,6 @@ import {
   CarouselNext,
   CarouselPrevious
 } from "@/components/ui/carousel"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 import { frameworks, languages, RenderIcon, tools } from "./technologies"
 
@@ -20,19 +19,13 @@ export const TechStack = () => (
     transition={{ duration: 0.5 }}
     className="mt-16"
   >
-    <Tabs defaultValue="all">
-      <TabsList className="mb-4">
-        <TabsTrigger value="all">All</TabsTrigger>
-        <TabsTrigger value="favorite">Favorite</TabsTrigger>
-      </TabsList>
-
-      <TabsContent value="all" className="space-y-12">
-        {Object.entries({ languages, frameworks, tools }).map(
-          ([name, group], i) => (
-            <div key={name} className="">
-              <h2 className="mb-2 text-muted-foreground/50">
-                {name.toLocaleUpperCase()}
-              </h2>
+    <div className="space-y-12">
+      {Object.entries({ languages, frameworks, tools }).map(
+        ([name, group], i) => (
+          <div key={name} className="">
+            <h2 className="mb-2 text-muted-foreground/50">
+              {name.toLocaleUpperCase()}
+            </h2>
 
               <div className="!mt-0 flex flex-row flex-wrap gap-x-8 gap-y-12">
                 <Carousel
@@ -73,67 +66,15 @@ export const TechStack = () => (
                               className="absolute size-10 rounded-full opacity-25 blur-lg"
                             />
 
-                            <RenderIcon
-                              icon={icon}
-                              className="relative size-10"
-                            />
-                          </motion.div>
-
-                          <span className="absolute -bottom-2 mx-auto mt-2 translate-y-full text-center text-xs text-muted-foreground">
-                            {icon.title}
-                          </span>
-                        </div>
-                      </CarouselItem>
-                    ))}
-                  </CarouselContent>
-                </Carousel>
-              </div>
-            </div>
-          )
-        )}
-      </TabsContent>
-
-      <TabsContent
-        value="favorite"
-        className="flex flex-row flex-wrap gap-x-8 gap-y-12"
-      >
-        {[...languages, ...frameworks, ...tools]
-          .filter((icon) => icon.favorite)
-          .map((icon, index) => (
-            <div
-              key={icon.slug}
-              className="relative flex flex-col items-center"
-            >
-              <motion.div
-                key={icon.slug}
-                initial={{ opacity: 0, scale: 0 }}
-                whileInView={{ opacity: 1, scale: 1 }}
-                transition={{
-                  type: "spring",
-                  stiffness: 200,
-                  damping: 15,
-                  delay: index * 0.1
-                }}
-                viewport={{ once: true }}
-                style={{
-                  backgroundColor: `color-mix(in lch, #${icon.hex} 10%, transparent)`
-                }}
-                className="relative overflow-clip rounded p-3"
-              >
-                <div
-                  style={{ background: "#" + icon.hex }}
-                  className="absolute size-10 rounded-full opacity-25 blur-lg"
-                />
-
-                <RenderIcon icon={icon} className="relative size-10" />
-              </motion.div>
-
-              <span className="absolute -bottom-2 mx-auto mt-2 translate-y-full text-center text-xs text-muted-foreground">
-                {icon.title}
-              </span>
-            </div>
-          ))}
-      </TabsContent>
-    </Tabs>
+                      <RenderIcon icon={icon} className="relative size-10" />
+                    </motion.div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+            </Carousel>
+          </div>
+        )
+      )}
+    </div>
   </motion.section>
 )

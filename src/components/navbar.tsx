767b06f99884d27ef2c4efbfb713ffbed78616cc
John P <johnrprutton@gmail.com>
2024-07-10 13:17:52 +0200

    refactor: navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,34 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 
-import { LucideMenu } from "lucide-react"
-
 import { Button } from "./ui/button"
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetHeader,
-  SheetTrigger
-} from "./ui/sheet"
-
-const links = (
-  [
-    { label: "About", href: "/about" },
-    { label: "Projects", href: "/projects" },
-    { label: "Contact", href: "/contact" }
-  ] as const
-).map((link) => (
-  <Button variant="ghost" asChild key={link.href}>
-    <Link href={link.href}>{link.label}</Link>
-  </Button>
-))
-
-const MenuButton = () => (
-  <Button size="icon" variant="ghost">
-    <LucideMenu />
-  </Button>
-)
 
 const Logo = () => (
   <Button
@@ -58,29 +31,9 @@ export const Navbar = () => (
     <nav className="container flex flex-row items-center justify-between py-4">
       <Logo />
 
-      <div className="hidden flex-row space-x-4 md:flex">{links}</div>
-
-      <Sheet>
-        <SheetTrigger asChild className="md:hidden">
-          <MenuButton />
-        </SheetTrigger>
-
-        <SheetContent className="p-4">
-          <SheetHeader className="mb-8">
-            <SheetClose className="ml-auto" asChild>
-              <MenuButton />
-            </SheetClose>
-          </SheetHeader>
-
-          <div className="flex flex-col gap-2">
-            {links.map((b) => (
-              <SheetClose key={b.key} asChild className="w-fit">
-                {b}
-              </SheetClose>
-            ))}
-          </div>
-        </SheetContent>
-      </Sheet>
+      <Button>
+        <Link href="mailto:johnrprutton@gmail.com">Contact Me</Link>
+      </Button>
     </nav>
   </div>
 )
